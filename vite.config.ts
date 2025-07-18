6dcd80e389960963de00d37108b5efb59cd575d1
jws <ws.jung@catenoid.net>
2025-07-18 16:11:02 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 이 부분을 추가
+  build: {
+    outDir: 'docs', // 빌드 결과물이 생성될 폴더
+  },
 })
